13827bc52fc818d6ae030a43e7edc546019019d4
Brad Bambara <brad.bambara@gmail.com>
2013-03-05 23:29:56 -0500

    adding scaling which fits art pig projects to the screen

--- Generated/ArtpigProj.cpp
+++ Generated/ArtpigProj.cpp
@@ -25,6 +25,20 @@ void ArtpigProj::load()
 	//set our node to be the symbol's root node
 	node = symbol->getRootNode();
 	
+	//calculate the scale that we should use to display the APS project
+	cocos2d::CCSize windowSize = cocos2d::CCDirector::sharedDirector()->getWinSize();
+	cocos2d::CCSize projSize = node->getContentSize();
+	float newScale = windowSize.width/projSize.height;//projSize height and width are switched because rotation is always +90
+	if(windowSize.height/projSize.width > newScale)
+		newScale = windowSize.height/projSize.width;
+	
+	//make the Cocos2dXML object reflect the properties set by the APS project
+	setPosition(node->getPosition());
+	setRotation(node->getRotation());
+	setAnchorPoint(node->getAnchorPoint());
+	setSize(cocos2d::CCPoint(node->getContentSize().width, node->getContentSize().height));
+	setScale(cocos2d::CCPoint(newScale, newScale));
+	
 	ArtpigProj_Base::load();
 	
 	//are we going to auto-play?
